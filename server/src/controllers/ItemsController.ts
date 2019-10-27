247c36fcee08298b5a93f19066a235ff2e17b33b
yngvarr <yngvarr.bel@gmail.com>
2019-10-27 16:50:08 +0300

    Added ItemsController

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { UserDao } from '@daos';
+import { logger } from '@shared';
+
+const itemsDao = new UserDao();
+
+export async function getAllItems(req: Request, res: Response){
+    try {
+        const items = await itemsDao.getAll();
+        return res.status(OK).json({items});
+    } catch (err) {
+        logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    }
+};
