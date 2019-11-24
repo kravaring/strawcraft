9a5f552d165d8a501782b2d964c4e266cc83e7f1
yngvarr <yngvarr.bel@gmail.com>
2019-11-24 15:30:14 +0300

    Changed server port and added proxy to client

--- server/src/start.ts
+++ server/src/start.ts
@@ -2,7 +2,7 @@ import app from '@server';
 import { logger } from '@shared';
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 9000);
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const loadData = async () => {
+    const response = await fetch('/api/items');
+    const items = await response.json();
+    console.log(items);
+};
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
