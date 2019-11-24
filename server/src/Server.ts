273b18a4f32adb24d288b12fb2e432e8ae1bbe94
yngvarr <yngvarr.bel@gmail.com>
2019-11-24 16:59:04 +0300

    Changed error handling

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,17 +1,37 @@
 import cookieParser from 'cookie-parser';
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import logger from 'morgan';
+import { logger as log } from '@shared';
 import BaseRouter from './routes';
+import { ApiError } from './models/ApiError';
 
 // Init express
 const app = express();
 
 // Add middleware/settings/routes to express.
+app.disable('x-powered-by');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use('/api', BaseRouter);
+app.use('/api', (error: Error, request: Request, response: Response, next: NextFunction ) => {
+    console.log('central err handling')
+    log.error(error);
+    if(response.headersSent){
+        return next(error);
+    }
+    if(error instanceof ApiError){
+        response.status(error.status);
+        response.json({error: error.message});
+    } else {
+        response.status(500);
+        const resJSON = process.env.NODE_ENV === 'development' ? 
+            {error: 'Internal server error', stack: error.stack} :
+            {error: 'Internal server error'};
+        response.json(resJSON);
+    }
+});
 
 // Export express instance
 export default app;
--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,38 +1,32 @@
 import { Request, Response } from 'express';
 import { BAD_REQUEST, CREATED, OK, NO_CONTENT } from 'http-status-codes';
 import { StrawItemDao } from '@daos';
-import { logger } from '@shared';
 import { Item } from '@entities';
+import { ApiError } from 'src/models/ApiError';
+import { NextFunction } from 'connect';
 
 const itemsDao = new StrawItemDao();
 
-function onError(err: Error, res: Response){
-    logger.error(err.message, err);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
-}
-
-export async function getAllItems(req: Request, res: Response){
+export async function getAllItems(req: Request, res: Response, next: NextFunction){
     try {
         const items = await itemsDao.getAll();
         return res.status(OK).json({items});
     } catch (err) {
-        return onError(err, res);
+        next(new ApiError(err.message, BAD_REQUEST));
     }
 }
 
-export async function saveItem(req: Request, res: Response){
+export async function saveItem(req: Request, res: Response, next: NextFunction){
     const item = req.body['item'] as Item;
     try{
         const savedItem = await itemsDao.add(item);
         return res.status(CREATED).json(savedItem);
     } catch(err){
-        return onError(err, res);
+        next(new ApiError(err.message, BAD_REQUEST));
     }
 }
 
-export async function deleteItem(req: Request, res: Response){
+export async function deleteItem(req: Request, res: Response, next: NextFunction){
     const id = Number(req.params.id);
     try{
         const deletedItem = await itemsDao.delete(id);
@@ -42,16 +36,16 @@ export async function deleteItem(req: Request, res: Response){
             return res.status(NO_CONTENT);
         }
     } catch(err){
-        return onError(err, res);
+        next(new ApiError(err.message, BAD_REQUEST));
     }
 }
 
-export async function updateItem(req: Request, res: Response){
+export async function updateItem(req: Request, res: Response, next: NextFunction){
     const item = req.body['item'] as Item;
     try{
         const updatedItem = await itemsDao.update(item);
         return res.status(OK).json(updatedItem);
     } catch(err){
-        return onError(err, res);
+        next(new ApiError(err.message, BAD_REQUEST));
     }
 }
